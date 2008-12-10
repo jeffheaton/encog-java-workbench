70a47a7cc10a31e071c621a85dbe30897ae8f8cc
Jeff Heaton <jeff@jeffheaton.com>
2008-12-10 00:27:40 +0000

    Workbench: added startup scripts
    git-svn-id: http://encog-java.googlecode.com/svn/trunk/encog-workbench@198 f90f6e9a-ac51-0410-b353-d1b83c6f6923

---
+++ workbench.bat
@@ -0,0 +1,1 @@
+start javaw -classpath ./jar/encog-core.jar;./jar/encog-workbench.jar;./jar/jfreechart-1.0.11.jar;./jar/jcommon-1.0.14.jar org.encog.workbench.EncogWorkBench
---
+++ workbench.sh
@@ -0,0 +1,1 @@
+java -classpath ./jar/encog-core.jar:./jar/encog-workbench.jar:./jar/jfreechart-1.0.11.jar:./jar/jcommon-1.0.14.jar org.encog.workbench.EncogWorkBench
