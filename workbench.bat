85050af78280be77c402e5c5f13d7c0521f06923
Seema Singh <Seemasingh8614@yahoo.com>
2009-05-13 15:50:33 +0000

    removed unneeded files from the workbench
    git-svn-id: http://encog-java.googlecode.com/svn/trunk/encog-workbench@592 f90f6e9a-ac51-0410-b353-d1b83c6f6923

--- workbench.bat
+++
@@ -1,1 +0,0 @@
-start javaw -classpath ./jar/encog-core.jar;./jar/encog-workbench.jar;./jar/jfreechart-1.0.11.jar;./jar/jcommon-1.0.14.jar org.encog.workbench.EncogWorkBench
--- workbench.sh
+++
@@ -1,1 +0,0 @@
-java -classpath ./jar/encog-core.jar:./jar/encog-workbench.jar:./jar/jfreechart-1.0.11.jar:./jar/jcommon-1.0.14.jar org.encog.workbench.EncogWorkBench
