859eaedbe41d58f014ac519995bd8f8233e171ac
Jeff Heaton <jeff@jeffheaton.com>
2009-08-29 14:17:27 +0000

    Beginning to build more advanced query window
    git-svn-id: http://encog-java.googlecode.com/svn/trunk/encog-workbench@887 f90f6e9a-ac51-0410-b353-d1b83c6f6923

---
+++ src/org/encog/workbench/frames/query/ThermalGraphicQuery.java
@@ -0,0 +1,26 @@
+package org.encog.workbench.frames.query;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseEvent;
+import java.awt.event.WindowEvent;
+
+import org.encog.workbench.frames.manager.EncogCommonFrame;
+
+public class ThermalGraphicQuery extends EncogCommonFrame {
+
+	public void windowOpened(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
