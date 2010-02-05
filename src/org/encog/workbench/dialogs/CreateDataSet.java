9b63399608303d07ad05d5297b46277413121a8e
Seema Singh <Seemasingh8614@yahoo.com>
2010-02-05 02:14:47 +0000

    Update header for 2.3
    git-svn-id: http://encog-java.googlecode.com/svn/trunk/encog-workbench@1259 f90f6e9a-ac51-0410-b353-d1b83c6f6923

--- src/org/encog/workbench/dialogs/CreateDataSet.java
+++ src/org/encog/workbench/dialogs/CreateDataSet.java
@@ -1,11 +1,11 @@
 /*
- * Encog Workbench v2.x
+ * Encog(tm) Workbench v2.3
  * http://www.heatonresearch.com/encog/
  * http://code.google.com/p/encog-java/
  * 
- * Copyright 2008-2009, Heaton Research Inc., and individual contributors.
- * See the copyright.txt in the distribution for a full listing of 
- * individual contributors.
+ * Copyright 2008-2010 by Heaton Research Inc.
+ * 
+ * Released under the LGPL.
  *
  * This is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as
@@ -21,7 +21,13 @@
  * License along with this software; if not, write to the Free
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ * 
+ * Encog and Heaton Research are Trademarks of Heaton Research, Inc.
+ * For information on Heaton Research trademarks, visit:
+ * 
+ * http://www.heatonresearch.com/copyright.html
  */
+
 package org.encog.workbench.dialogs;
 
 import java.awt.Container;
