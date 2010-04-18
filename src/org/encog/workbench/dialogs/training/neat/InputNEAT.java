d5e6bc9ceed95b218aa07639eb644779092c4389
Seema Singh <Seemasingh8614@yahoo.com>
2010-04-18 00:20:36 +0000

    Added fields to NEAT training dialog
    git-svn-id: http://encog-java.googlecode.com/svn/trunk/encog-workbench@1502 f90f6e9a-ac51-0410-b353-d1b83c6f6923

--- src/org/encog/workbench/dialogs/training/neat/InputNEAT.java
+++ src/org/encog/workbench/dialogs/training/neat/InputNEAT.java
@@ -13,12 +13,30 @@ import org.encog.workbench.EncogWorkBench;
 import org.encog.workbench.dialogs.common.ComboBoxField;
 import org.encog.workbench.dialogs.common.DoubleField;
 import org.encog.workbench.dialogs.common.EncogPropertiesDialog;
+import org.encog.workbench.dialogs.common.IntegerField;
 
 public class InputNEAT extends EncogPropertiesDialog {
 	
-	private ComboBoxField comboTraining;
-	private ComboBoxField comboPopulation;
-	private ComboBoxField comboNetwork;
+	private final ComboBoxField comboTraining;
+	private final ComboBoxField comboPopulation;
+	private final ComboBoxField comboNetwork;
+		
+	private final DoubleField activationMutationRate;
+	private final DoubleField chanceAddLink;
+	private final DoubleField chanceAddNode;
+	private final DoubleField chanceAddRecurrentLink;
+	private final DoubleField compatibilityThreshold;
+	private final DoubleField crossoverRate;
+	private final DoubleField maxActivationPerturbation;
+	private final IntegerField maxNumberOfSpecies;
+	private final DoubleField maxPermittedNeurons;
+	private final DoubleField maxWeightPerturbation;
+	private final DoubleField mutationRate;
+	private final IntegerField numAddLinkAttempts;
+	private final IntegerField numGensAllowedNoImprovement;
+	private final IntegerField numTrysToFindLoopedLink;
+	private final IntegerField numTrysToFindOldLink;
+	private final DoubleField probabilityWeightReplaced;
 	
 	/**
 	 * Text field that holds the maximum training error.
@@ -49,12 +67,31 @@ public class InputNEAT extends EncogPropertiesDialog {
 		super(owner);
 		findData();
 		setTitle("NeuroEvolution of Augmenting Topologies (NEAT)");
-		setSize(400,400);
+		setSize(400,500);
 		setLocation(200,200);
 		addProperty(this.comboTraining = new ComboBoxField("training set","Training Set",true,this.trainingSets));
 		addProperty(this.comboPopulation = new ComboBoxField("population","Population",true,this.populations));
 		addProperty(this.comboNetwork = new ComboBoxField("network","Base Network",true,this.networks));
 		addProperty(this.maxError = new DoubleField("max error","Maximum Error",true,0,1));
+		
+		addProperty(this.activationMutationRate=new DoubleField("activation mutation rate","Activation Mutation Rate",true,0,1));
+		addProperty(this.chanceAddLink=new DoubleField("chance add link","Chance to Add Link",true,0,1));
+		addProperty(this.chanceAddNode=new DoubleField("chance add node","Chance to Add Node",true,0,1));
+		addProperty(this.chanceAddRecurrentLink=new DoubleField("chance add recur link","Chance to Add Recurrent Link",true,0,1));
+		addProperty(this.compatibilityThreshold=new DoubleField("compat threshold","Species Compat Threshold",true,0,1));
+		addProperty(this.crossoverRate=new DoubleField("crossover rate","Crossover Rate",true,0,1));
+		addProperty(this.maxActivationPerturbation=new DoubleField("max act pert","Max Activation Perturb",true,0,1));
+		addProperty(this.maxNumberOfSpecies=new IntegerField("max num species","Max Num Species(0=none)",true,0,1));
+		addProperty(this.maxPermittedNeurons=new DoubleField("max neurons","Max Neurons",true,0,1));
+		addProperty(this.maxWeightPerturbation=new DoubleField("max weight pert","Max Weight Perturb",true,0,1));
+		addProperty(this.mutationRate=new DoubleField("mutation rate","Mutation Rate",true,0,1));
+		addProperty(this.numAddLinkAttempts=new IntegerField("link add attempts","Num Link Attempts",true,0,1));
+		addProperty(this.numGensAllowedNoImprovement=new IntegerField("max gens no improve","Max Generations w/No Improvement",true,0,1));
+		addProperty(this.numTrysToFindLoopedLink=new IntegerField("tries for looped link","Tries for Linked Loop",true,0,1));
+		addProperty(this.numTrysToFindOldLink=new IntegerField("tries find old link","Tries to Find Old Link",true,0,1));
+		addProperty(this.probabilityWeightReplaced=new DoubleField("prob weight replaced","Chance Weight Replaced",true,0,1));
+		
+		
 		render();
 	}
 
@@ -106,6 +143,7 @@ public class InputNEAT extends EncogPropertiesDialog {
 	public DoubleField getMaxError() {
 		return maxError;
 	}
+	
 	
 	
 }
