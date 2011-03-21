89225a9dba098dc3997aaadd64220a60610d294c
Seema Singh <Seemasingh8614@yahoo.com>
2011-03-21 14:18:11 +0000

    Small fix to workbench OCR tab
    git-svn-id: http://encog-java.googlecode.com/svn/trunk/encog-workbench@2872 f90f6e9a-ac51-0410-b353-d1b83c6f6923

--- src/org/encog/workbench/tabs/query/ocr/OCRQueryTab.java
+++ src/org/encog/workbench/tabs/query/ocr/OCRQueryTab.java
@@ -12,6 +12,7 @@ import javax.swing.JTable;
 
 import org.encog.EncogError;
 import org.encog.ml.BasicML;
+import org.encog.ml.MLClassification;
 import org.encog.ml.MLOutput;
 import org.encog.ml.MLRegression;
 import org.encog.neural.data.NeuralData;
@@ -32,75 +33,95 @@ public class OCRQueryTab extends EncogCommonTab implements ActionListener {
 	private JButton buttonClear;
 	private DrawingEntry entry;
 	private JTable outputTable;
+	private boolean classification;
 
-	
 	public OCRQueryTab(BasicML method) {
-		super((EncogPersistedObject)method);
-		
+		super((EncogPersistedObject) method);
+
+		this.classification = method instanceof MLClassification;
+
 		this.method = method;
-		
+
 		this.setLayout(new BorderLayout());
 		JPanel body = new JPanel();
-		body.setLayout(new GridLayout(1,2));
-		this.add(body,BorderLayout.CENTER);
-		
+		body.setLayout(new GridLayout(1, 2));
+		this.add(body, BorderLayout.CENTER);
+
 		this.buttonQuery = new JButton("Query");
 		this.buttonDownsample = new JButton("Downsample");
-		this.buttonClear = new JButton("Clear");		
+		this.buttonClear = new JButton("Clear");
 		this.buttonPanel = new JPanel();
 		this.buttonPanel.add(this.buttonQuery);
 		this.buttonPanel.add(this.buttonDownsample);
 		this.buttonPanel.add(this.buttonClear);
 		this.add(this.buttonPanel, BorderLayout.NORTH);
-		
+
 		this.buttonQuery.addActionListener(this);
 		this.buttonDownsample.addActionListener(this);
 		this.buttonClear.addActionListener(this);
-		
+
 		this.panel = new OCRGridPanel(this.method);
 		this.entry = new DrawingEntry();
-		
+
 		JPanel panelLeft = new JPanel();
 		JPanel panelRight = new JPanel();
-		panelLeft.setLayout(new GridLayout(2,1));
+		panelLeft.setLayout(new GridLayout(2, 1));
 		panelLeft.add(this.panel);
 		panelLeft.add(this.entry);
-				
+
 		body.add(panelLeft);
 		body.add(panelRight);
-		
+
 		panelRight.setLayout(new BorderLayout());
-		
-		int outputCount = ((MLOutput)method).getOutputCount();
 
-		panelRight.add(this.outputTable = new JTable(new NetworkQueryModel(
-				outputCount, 2)), BorderLayout.CENTER);
-		this.outputTable.setEnabled(false);
+		if (classification) {
+			int outputCount = 1;
 
-		for (int i = 1; i <= outputCount; i++) {
-			this.outputTable.setValueAt("Output " + i + ":", i - 1, 0);
-			this.outputTable.setValueAt("0.0", i - 1, 1);
-		}		
+			panelRight.add(this.outputTable = new JTable(new NetworkQueryModel(
+					outputCount, 2)), BorderLayout.CENTER);
+			this.outputTable.setEnabled(false);
+
+			this.outputTable.setValueAt("Classification Output:", 0, 0);
+			this.outputTable.setValueAt("0.0", 0, 1);
+
+		} else {
+			int outputCount = ((MLOutput) method).getOutputCount();
+
+			panelRight.add(this.outputTable = new JTable(new NetworkQueryModel(
+					outputCount, 2)), BorderLayout.CENTER);
+			this.outputTable.setEnabled(false);
+
+			for (int i = 1; i <= outputCount; i++) {
+				this.outputTable.setValueAt("Output " + i + ":", i - 1, 0);
+				this.outputTable.setValueAt("0.0", i - 1, 1);
+			}
+		}
 	}
-	
+
 	public void performDownsample() {
-		boolean[] data = this.entry.downSample(this.panel.getGridWidth(),this.panel.getGridHeight());
+		boolean[] data = this.entry.downSample(this.panel.getGridWidth(),
+				this.panel.getGridHeight());
 		this.panel.setGrid(data);
 	}
-	
+
 	public void performQuery() {
 		try {
-		int outputCount = ((MLOutput)method).getOutputCount();
-		boolean[] grid = this.panel.getGrid();
-		NeuralData input = new BasicNeuralData(grid.length);
-		for(int i=0;i<grid.length;i++) {
-			input.setData(i, grid[i]?1:-1);
-		}
+			int outputCount = ((MLOutput) method).getOutputCount();
+			boolean[] grid = this.panel.getGrid();
+			NeuralData input = new BasicNeuralData(grid.length);
+			for (int i = 0; i < grid.length; i++) {
+				input.setData(i, grid[i] ? 1 : -1);
+			}
 
-		NeuralData output = ((MLRegression)this.method).compute(input);
-		for (int i = 0; i < outputCount; i++) {
-			this.outputTable.setValueAt(output.getData(i), i, 1);
-		}
+			if (classification) {
+				int output = ((MLClassification) this.method).classify(input);
+				this.outputTable.setValueAt(output, 0, 1);
+			} else {
+				NeuralData output = ((MLRegression) this.method).compute(input);
+				for (int i = 0; i < outputCount; i++) {
+					this.outputTable.setValueAt(output.getData(i), i, 1);
+				}
+			}
 		} catch (EncogError ex) {
 			EncogWorkBench.displayError("Query Error", ex.getMessage());
 		} catch (Throwable t) {
@@ -108,18 +129,19 @@ public class OCRQueryTab extends EncogCommonTab implements ActionListener {
 		}
 	}
 
-
 	public void actionPerformed(ActionEvent e) {
-		if( e.getSource()==this.buttonClear ) {
+		if (e.getSource() == this.buttonClear) {
 			this.panel.clear();
 			this.entry.clear();
-		} if( e.getSource()==this.buttonDownsample) {
+		}
+		if (e.getSource() == this.buttonDownsample) {
 			performDownsample();
-		} if( e.getSource()==this.buttonQuery ) {
+		}
+		if (e.getSource() == this.buttonQuery) {
 			performDownsample();
 			performQuery();
 		}
-		
+
 	}
 
 }
