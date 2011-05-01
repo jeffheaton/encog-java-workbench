41a6d630667a676ac12967467f9fe23fd7fdd071
Jeff Heaton <jeff@jeffheaton.com>
2011-05-01 15:26:35 +0000

    Added row/col to editors.
    git-svn-id: http://encog-java.googlecode.com/svn/trunk/encog-workbench@3378 f90f6e9a-ac51-0410-b353-d1b83c6f6923

--- src/main/java/org/encog/workbench/tabs/files/text/BasicTextTab.java
+++ src/main/java/org/encog/workbench/tabs/files/text/BasicTextTab.java
@@ -8,7 +8,13 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 
+import javax.swing.JLabel;
 import javax.swing.JScrollPane;
+import javax.swing.event.CaretEvent;
+import javax.swing.event.CaretListener;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.JTextComponent;
+import javax.swing.text.Utilities;
 
 import org.encog.workbench.EncogWorkBench;
 import org.encog.workbench.WorkBenchError;
@@ -16,11 +22,12 @@ import org.encog.workbench.frames.document.tree.ProjectFile;
 import org.encog.workbench.tabs.files.BasicFileTab;
 import org.encog.workbench.util.EncogFonts;
 
-public class BasicTextTab extends BasicFileTab implements ComponentListener {
+public class BasicTextTab extends BasicFileTab implements ComponentListener, CaretListener {
 
 	private final NonWrappingTextPane editor;
 	private final JScrollPane scroll;
 	private final BasicTextDocListener dirty = new BasicTextDocListener(this);
+	private final JLabel status = new JLabel();
 
 	public BasicTextTab(ProjectFile file) {
 		super(file);
@@ -28,11 +35,13 @@ public class BasicTextTab extends BasicFileTab implements ComponentListener {
 		this.editor = new NonWrappingTextPane();				
 		this.editor.setFont(EncogFonts.getInstance().getCodeFont());
 		this.editor.setEditable(true);
+		this.editor.addCaretListener(this);
 
 		this.setLayout(new BorderLayout());
 		this.scroll = new JScrollPane(this.editor);
 		add(this.scroll, BorderLayout.CENTER);
 		this.addComponentListener(this);
+		add(this.status,BorderLayout.SOUTH);		
 		loadFile();
 	}
 
@@ -108,6 +117,42 @@ public class BasicTextTab extends BasicFileTab implements ComponentListener {
 
 	public BasicTextDocListener getDirty() {
 		return dirty;
+	}
+	
+	public static int getRow(int pos, JTextComponent editor) {
+        int rn = (pos==0) ? 1 : 0;
+        try {
+            int offs=pos;
+            while( offs>0) {
+                offs=Utilities.getRowStart(editor, offs)-1;
+                rn++;
+            }
+        } catch (BadLocationException e) {
+            e.printStackTrace();
+        }
+        return rn;
+    }
+
+    public static int getColumn(int pos, JTextComponent editor) {
+        try {
+            return pos-Utilities.getRowStart(editor, pos)+1;
+        } catch (BadLocationException e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
+
+
+	@Override
+	public void caretUpdate(CaretEvent e) {
+		StringBuilder s = new StringBuilder();
+		s.append("Row: ");
+		int pos = this.editor.getCaretPosition();
+		s.append(getRow(pos,this.editor));
+		s.append(", Col: ");
+		s.append(getColumn(pos,this.editor));
+		this.status.setText(s.toString());
+		
 	}
 	
 	
