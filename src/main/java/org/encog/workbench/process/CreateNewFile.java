fe24371fa410c3960a8b08e62b35dd206a7a9cb8
Seema Singh <Seemasingh8614@yahoo.com>
2011-08-02 14:58:31 -0500

    Fixed compile error in workbench

--- src/main/java/org/encog/workbench/process/CreateNewFile.java
+++ src/main/java/org/encog/workbench/process/CreateNewFile.java
@@ -120,9 +120,9 @@ public class CreateNewFile {
 			int populationSize = dialog.getPopulationSize().getValue();
 			int inputCount = dialog.getInputNeurons().getValue();
 			int outputCount = dialog.getOutputNeurons().getValue();
-			boolean snapshot = dialog.getSnapshot().getValue();
+			int cycles = dialog.getActivationCycles().getValue();
 			NEATPopulation pop = new NEATPopulation(inputCount,outputCount,populationSize);
-			pop.setSnapshot(snapshot);
+			pop.setActivationCycles(cycles);
 			pop.setNeatActivationFunction(dialog.getNeatActivationFunction());
 			pop.setOutputActivationFunction(dialog.getOutputActivationFunction());
 			EncogWorkBench.getInstance().save(path,pop);
